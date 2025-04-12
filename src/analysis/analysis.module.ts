d444cc66e65ef55cf50756565f7df07b3a515165
Andrew da Silva <andrewairamdasilva@gmail.com>
2025-04-11 23:00:22 -0300

    feat(analysis): add analysis curriculum module

--- src/analysis/analysis.module.ts
+++ src/analysis/analysis.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { CurriculumAnalysisService } from './core/service/curriculum-analysis.service';
+import { ExternalAnalyseProcessing } from './http/client/external-analyse-processing.client';
+import { ExternalEvaluationProcessing } from './http/client/external-evaluation-processing.client';
+import { PrismaModule } from '../prisma/prisma.module';
+import MatchingAnalyzerController from './http/rest/controller/matching-analyser.controller';
+import { ConfigModule } from '../config/config.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), PrismaModule],
+  providers: [
+    CurriculumAnalysisService,
+    ExternalAnalyseProcessing,
+    ExternalEvaluationProcessing,
+  ],
+  controllers: [MatchingAnalyzerController],
+  exports: [CurriculumAnalysisService],
 })
 export class AnalysisModule {}
